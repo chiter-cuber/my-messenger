f08a9971f0bd386eebc64c1b5b9c3f2c1d62c7ba
chiter-cuber <goldic999@gmail.com>
2026-06-30 00:22:35 +0500

    Fix: add render_template import

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
 import sqlite3
 import os
-
+                                                    
 app = Flask(__name__)
 DB_NAME = 'chat.db'
 
